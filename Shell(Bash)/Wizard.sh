0048f324a20abc85b91e03803605844181bfc6af
CyberChef <94846646+TCyberChef@users.noreply.github.com>
2023-02-27 20:11:33 +0200

    .

--- Shell(Bash)/Wizard.sh
+++ Shell(Bash)/Wizard.sh
@@ -10,3 +10,46 @@
 # The game is written in bash, so I had limited features. But I went all out, 
 # and I always want to add features and renew the game.
 ###
+
+
+
+
+### greetings.
+
+read -rep "Welcome, Hero! " manual_0
+read -rep 'I am so happy to see you here again ! ' manual_1
+read -rep '' manual_1
+read -rep '' manual_1
+read -rep '' manual_1
+read -rep '' manual_1
+read -rep '' manual_1
+
+
+
+
+read -rep "So, What is your choice ? 
+    Are you are not brave enough ? " first_step
+case $first_step in
+OK|ok)
+
+;;
+
+NO|no)
+
+;;
+
+*)
+echo 'you chicken ! '
+exit
+;;
+esac
+
+echo -en 'Once upon a time, there was a great wizard.
+the wizard would have traveled along the woods and the mountains.'
+read -r woke_up
+echo
+echo 'One day in one of his walks the wizard found a mangeta lizard ! '
+read -r lizard
+read -rep "Continue? "  answer
+echo "${answer}"
+
